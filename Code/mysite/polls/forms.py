7cae6dcce9d0f01e2c36a11b39c8bd7dd1f27244
GonuguntlaHarichandana <gonuguntla.harichandana19@st.niituniversity.in>
2022-12-02 12:50:38 +0530

    Web applicatiob commit

---
+++ Code/mysite/polls/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Inputemo
+
+class EmoForm(forms.ModelForm):
+    class Meta:
+        model = Inputemo
+        fields = ('emo_name', 'file')
+        
---
+++ start.py
@@ -0,0 +1,4 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+driver.get("http://127.0.0.1:8000/home/upload")
